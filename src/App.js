c144263214173b405390919cf25952e696cf3dab
Kavya Kumar <tellkavya@gmail.com>
2019-09-19 11:29:51 -0700

    Add date, avatar, and addbutton files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import TaskList from './TaskList';
+import Date from './Date';
+import Avatar from './Avatar';
+import AddButton from './AddButton';
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello World!
+      <div style={{ padding: '30px 30px' }}>
+        <Avatar />
+        <br />
+        <Date />
+        <br/>
+        < TaskList />
+        <br />
+        <AddButton />
       </div>
     );
   }
