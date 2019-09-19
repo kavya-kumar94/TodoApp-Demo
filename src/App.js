e6f7b974982502b0050de9d2eb3c88e2aa82fbb3
Kavya Kumar <tellkavya@gmail.com>
2019-09-19 12:41:49 -0700

    Modify tasklist

--- src/App.js
+++ src/App.js
@@ -5,6 +5,47 @@ import Date from './Date';
 import Avatar from './Avatar';
 import AddButton from './AddButton';
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [
+        {
+          'time': '12',
+          'period': 'AM',
+          'activity_title': 'Finish Tutorial Series',
+          'activity_description': '#ReactForNewbies'
+        }, {
+          'time': '9',
+          'period': 'AM',
+          'activity_title': 'Meeting with Team Leads',
+          'activity_description': 'New Project Kickoff'
+        }, {
+          'time': '11',
+          'period': 'AM',
+          'activity_title': 'Call Mom',
+          'activity_description': 'Return her call before she kills me'
+        }, {
+          'time': '3',
+          'period': 'PM',
+          'activity_title': 'Fix Wifey\'s website',
+          'activity_description': 'FB Ads Integration not working'
+        }, {
+          'time': '6',
+          'period': 'PM',
+          'activity_title': 'Do DB Backups',
+          'activity_description': 'Related to upcoming server migration'
+        }
+      ]
+    }
+    this.addTask = this.addTask.bind(this);
+  }
+
+  addTask() {
+    var task = { 'time': '5', 'period': 'AM', 'activity_title': 'Jogging', 'activity_description': 'Go for a run!' };
+    var tasks = this.state.tasks.concat(task);
+    this.setState({ tasks: tasks });
+  }
+
   render() {
     return (
       <div style={{ padding: '30px 30px' }}>
@@ -12,9 +53,9 @@ class App extends Component {
         <br />
         <Date />
         <br/>
-        < TaskList />
+        < TaskList tasks = {this.state.tasks}/>
         <br />
-        <AddButton />
+        <AddButton onClick={this.addTask.bind(this)} />
       </div>
     );
   }
